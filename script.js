4c00ef405cf571f4fce8782c466f4ba3f056f0eb
GunnerStrike <panjannejc11@gmail.com>
2020-10-29 17:30:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+        document.getElementById("principal2").innerHTML = principal;
+
+    var rate = document.getElementById("rate").value;
+        document.getElementById("rate2").innerHTML = rate;  
+
+         
     
 }
-        
+
+var principal = document.getElementById("principal");
+var rate = document.getElementById("rate");
+var years = document.getElementById("years");
+var interest = principal * rate * years / 100;
+
+    document.getElementById("interest2").innerHTML = interest; 
+
+var slider = document.getElementById("rate");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
